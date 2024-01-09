70010d01f193e65fa540210ba77e8117c52cb47e
pradeep-sigamani <pradeep@tritonadc.in>
2024-01-09 11:32:08 +0530

    updated filter option foe mobile checklist

--- src/service/mobile/checklist/list/index.js
+++ src/service/mobile/checklist/list/index.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const model = require("../../../../models/index");
 const objectId = mongoose.Types.ObjectId;
+const { filterByOption } = require("../../../../utils/enum");
+const moment = require("moment");
 
 const checkListMobile = async (query) => {
   const {
@@ -10,9 +12,9 @@ const checkListMobile = async (query) => {
     sortkey,
     sortOrder,
     activityId,
-    equipmentTag,
+    equipmentTagId,
     activityType,
-    filterType
+    filterBy,
   } = query;
 
   const sort = { [sortkey]: !sortOrder || sortOrder === "DESC" ? -1 : 1 };
@@ -20,42 +22,43 @@ const checkListMobile = async (query) => {
   const searchRegex = new RegExp(["^.*", searchKey, ".*$"].join(""), "i");
 
   let filter;
+  let filterByQuery;
 
-  if (activityId && !equipmentTag && !activityType) {
+  if (activityId && !equipmentTagId && !activityType) {
     filter = {
       $match: { activity_id: new objectId(activityId), delete_status: false },
     };
-  } else if (!activityId && equipmentTag && !activityType) {
+  } else if (!activityId && equipmentTagId && !activityType) {
     filter = {
       $match: {
-        equipment_tag: new objectId(equipmentTag),
+        equipment_tag: new objectId(equipmentTagId),
         delete_status: false,
       },
     };
-  } else if (!activityId && !equipmentTag && activityType) {
+  } else if (!activityId && !equipmentTagId && activityType) {
     filter = {
       $match: {
         activity_type: new objectId(activityType),
         delete_status: false,
       },
     };
-  } else if (activityId && equipmentTag && !activityType) {
+  } else if (activityId && equipmentTagId && !activityType) {
     filter = {
       $match: {
         activity_id: new objectId(activityId),
-        equipment_tag: new objectId(equipmentTag),
+        equipment_tag: new objectId(equipmentTagId),
         delete_status: false,
       },
     };
-  } else if (!activityId && equipmentTag && activityType) {
+  } else if (!activityId && equipmentTagId && activityType) {
     filter = {
       $match: {
-        equipment_tag: new objectId(equipmentTag),
+        equipment_tag: new objectId(equipmentTagId),
         activity_type: new objectId(activityType),
         delete_status: false,
       },
     };
-  } else if (activityId && !equipmentTag && activityType) {
+  } else if (activityId && !equipmentTagId && activityType) {
     filter = {
       $match: {
         activity_id: new objectId(activityId),
@@ -63,11 +66,11 @@ const checkListMobile = async (query) => {
         delete_status: false,
       },
     };
-  } else if (activityId && equipmentTag && activityType) {
+  } else if (activityId && equipmentTagId && activityType) {
     filter = {
       $match: {
         activity_id: new objectId(activityId),
-        equipment_tag: new objectId(equipmentTag),
+        equipment_tag: new objectId(equipmentTagId),
         activity_type: new objectId(activityType),
         delete_status: false,
       },
@@ -75,9 +78,46 @@ const checkListMobile = async (query) => {
   } else {
     filter = { $match: { delete_status: false } };
   }
-  console.log(filter);
+
+  const currentDate = moment();
+
+  if (filterBy === filterByOption.quarterly) {
+    filterByQuery = {
+      $match: {
+        check_list_type: filterByOption.quarterly,
+        date_of_create: { $lte: new Date(currentDate.toISOString()) },
+      },
+    };
+  } else if (filterBy === filterByOption.monthly) {
+    filterByQuery = {
+      $match: {
+        check_list_type: filterByOption.monthly,
+        date_of_create: { $lte: new Date(currentDate.toISOString()) },
+      },
+    };
+  } else if (filterBy === filterByOption.halfYearly) {
+    filterByQuery = {
+      $match: {
+        check_list_type: filterByOption.halfYearly,
+        date_of_create: { $lte: new Date(currentDate.toISOString()) },
+      },
+    };
+  } else if (filterBy === filterByOption.yearly) {
+    filterByQuery = {
+      $match: {
+        check_list_type: filterByOption.yearly,
+        date_of_create: { $lte: new Date(currentDate.toISOString()) },
+      },
+    };
+  } else {
+    filterByQuery = {
+      $match: { delete_status: false },
+    };
+  }
+
   const recordList = await model.checkListModel.aggregate([
     { ...filter },
+    { ...filterByQuery },
     {
       $match: searchKey
         ? {
