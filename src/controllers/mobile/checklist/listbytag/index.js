77d41d7ec0e09ed49dcf9fbd0f7164613c3dcebf
pradeep-sigamani <pradeep@tritonadc.in>
2024-02-23 14:44:11 +0530

    handled duplicate data in array

--- src/controllers/mobile/checklist/listbytag/index.js
+++ src/controllers/mobile/checklist/listbytag/index.js
@@ -6,10 +6,20 @@ const checklistListUsingTag = async (req, res, next) => {
   try {
     const record = await mobileServiceController.checklist.listByTag(req.query);
 
+    let uniqueArr = record.filter(
+      (item, index, self) =>
+        index ===
+        self.findIndex(
+          (t) =>
+            t.cobie_tag === item.cobie_tag &&
+            t.check_list_type === item.check_list_type
+        )
+    );
+
     return res.json({
       Status: "Success",
       Message: successMsg.GROUP_LIST_VIEWED_SUCCESSFULLY,
-      Data: record,
+      Data: uniqueArr,
       Code: 200,
     });
   } catch (error) {
