e4300968ef70e99ae2938151255a569c0ab107a5
pradeep-sigamani <pradeep@tritonadc.in>
2024-02-27 13:05:12 +0530

    updated cobie tag for update checklist validation

--- src/service/mobile/checklist/submit/index.js
+++ src/service/mobile/checklist/submit/index.js
@@ -4,7 +4,7 @@ const submitList = async (payload) => {
   const record = await model.submitchecklistModel.create({ ...payload });
   for (const iterator of payload.check_list_time) {
     const updateChecklist = await model.checkListValidation.findOneAndUpdate(
-      { equipment_tag: payload.equipment_tag_name },
+      { cobie_tag: payload.equipment_tag_name },
       { [iterator]: "checked" }
     );
   }
