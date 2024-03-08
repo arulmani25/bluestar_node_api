fb4e2f46a37e1ca52c03b06df9ebda047016fdcd
pradeep-sigamani <pradeep@tritonadc.in>
2024-03-08 18:21:21 +0530

    updated temp checklist

--- src/service/tempchecklist/get/index.js
+++ src/service/tempchecklist/get/index.js
@@ -1,14 +1,15 @@
+const mongoose = require("mongoose");
 const model = require("../../../models/index");
 const { errorMsg } = require("../../../utils");
 
 const getTempCheckListById = async (id) => {
-  const isExist = await model.tempCheckListModel.findOne({
+  const record = await model.tempCheckListModel.find({
     equipment_tag_name: id,
   });
-  if (!isExist) throw new Error(errorMsg.CHECK_LIST_NOT_FOUND);
-  const record = await model.tempCheckListModel.findOne({
-    equipment_tag_name: isExist.equipment_tag_name,
-  });
+  // if (!isExist) throw new Error(errorMsg.CHECK_LIST_NOT_FOUND);
+  // const record = await model.tempCheckListModel.findOne({
+  //   equipment_tag_name: isExist.equipment_tag_name,
+  // });
   return record;
 };
 
