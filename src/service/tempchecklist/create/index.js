6ae05917c0db43ae76b5eb4b6e312790d1c5ee10
pradeep-sigamani <pradeep@tritonadc.in>
2024-03-08 18:46:38 +0530

    updated temp chceklist datastore

--- src/service/tempchecklist/create/index.js
+++ src/service/tempchecklist/create/index.js
@@ -6,15 +6,20 @@ const createTempCheckList = async (payload) => {
     equipment_tag_name: payload.equipment_tag_name,
   });
   if (isEquipmentTagAvailable) {
-    for (const data of payload.data_store) {
-      const record = await model.tempCheckListModel.findOneAndUpdate(
-        { equipment_tag_name: isEquipmentTagAvailable.equipment_tag_name },
-        {
-          $push: { data_store: data },
-        }
-      );
-      return record;
-    }
+    // for (const data of payload.data_store) {
+    //   const record = await model.tempCheckListModel.findOneAndUpdate(
+    //     { equipment_tag_name: isEquipmentTagAvailable.equipment_tag_name },
+    //     {
+    //       $push: { data_store: data },
+    //     }
+    //   );
+    //   return record;
+    // }
+    const record = await model.tempCheckListModel.findOneAndUpdate(
+      { equipment_tag_name: isEquipmentTagAvailable.equipment_tag_name },
+      { ...payload }
+    );
+    return record;
   } else {
     const createRecord = await model.tempCheckListModel.create({ ...payload });
     return createRecord;
