3d5e0b6345a1d00b98eb0406f6b48262ced0c98b
pradeep-sigamani <pradeep@tritonadc.in>
2024-03-13 11:55:35 +0530

    updated response for sumbit checklist preview

--- src/controllers/mobile/checklist/listbytag/index.js
+++ src/controllers/mobile/checklist/listbytag/index.js
@@ -6,20 +6,10 @@ const checklistListUsingTag = async (req, res, next) => {
   try {
     const record = await mobileServiceController.checklist.listByTag(req.query);
 
-    let uniqueArr = record.filter(
-      (item, index, self) =>
-        index ===
-        self.findIndex(
-          (t) =>
-            t.cobie_tag === item.cobie_tag &&
-            t.check_list_type === item.check_list_type
-        )
-    );
-
     return res.json({
       Status: "Success",
       Message: successMsg.GROUP_LIST_VIEWED_SUCCESSFULLY,
-      Data: uniqueArr,
+      Data: record,
       Code: 200,
     });
   } catch (error) {
--- src/service/mobile/checklist/listbytag/index.js
+++ src/service/mobile/checklist/listbytag/index.js
@@ -3,9 +3,9 @@ const model = require("../../../../models/index");
 const listByTag = async (query) => {
   const endOfDay = new Date(query.date);
   endOfDay.setHours(23, 59, 59, 999);
-  const record = await model.checkListModel.find(
+  const record = await model.submitchecklistModel.find(
     {},
-    { cobie_tag: 1, check_list_type: 1, createdAt: 1 },
+    { _id: 0, equipment_tag_name: 1 },
     { sort: { createdAt: -1 } }
   );
   return record;
