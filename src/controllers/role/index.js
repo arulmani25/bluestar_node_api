a5ac8f64f705539c4023136a8639b7e340286d47
pradeep-sigamani <pradeep@tritonadc.in>
2024-03-20 12:34:17 +0530

    added role delete api

--- src/controllers/role/index.js
+++ src/controllers/role/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 const { createRoleType } = require("./create");
 const { getRoleTypeList } = require("./list");
+const { deleteRole } = require("./delete");
 const { verifyToken } = require("../../utils");
 
 router.post("/create", verifyToken, createRoleType);
 router.get("/list", verifyToken, getRoleTypeList);
+router.put("/delete/:id", verifyToken, deleteRole);
 
 module.exports = router;
--- src/service/role/index.js
+++ src/service/role/index.js
@@ -1,4 +1,5 @@
 const { createRole } = require("./create");
 const { roleTypeList } = require("./list");
+const { deleteRole } = require("./delete");
 
-module.exports = { createRole, roleTypeList };
+module.exports = { createRole, roleTypeList, deleteRole };
